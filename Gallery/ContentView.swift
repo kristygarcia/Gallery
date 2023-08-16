26ce1ba1d93a7cd65985275c26c39b252602bfe6
Kristy Garcia <kristymichgarcia@gmail.com>
2023-08-16 09:39:12 -0400

    Hi!

--- Gallery/ContentView.swift
+++ Gallery/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         HStack{
             VStack{
-                
+              Text("Hello, World")
             }
             VStack{
                 
