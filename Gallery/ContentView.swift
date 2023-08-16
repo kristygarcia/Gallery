fa8b000c364e6f4e8b76034986c6d116bc7e2d76
Kristy Garcia <kristymichgarcia@gmail.com>
2023-08-16 09:32:42 -0400

    Initial Commit

---
+++ Gallery/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Gallery
+//
+//  Created by scholar on 8/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack{
+            VStack{
+                
+            }
+            VStack{
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gallery/GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryApp.swift
+//  Gallery
+//
+//  Created by scholar on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
